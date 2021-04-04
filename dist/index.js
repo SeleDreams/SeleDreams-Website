689380216543845755fd6fb6b8d1df9dfbc9b048
LAMCILAK <lamcilaktheo@gmail.com>
2021-04-04 20:07:42 +0200

    Adjusted size of heading depending on device

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,8 @@
 "use strict";
-var message = "Hello World!";
-console.log(message);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsSUFBSSxPQUFPLEdBQVksY0FBYyxDQUFDO0FBQ3RDLE9BQU8sQ0FBQyxHQUFHLENBQUMsT0FBTyxDQUFDLENBQUMifQ==
+var heading = document.getElementById('heading-panel');
+var content = document.getElementById('content-panel');
+var nav = document.getElementById('main-navbar');
+var footer = document.getElementById('footer-panel');
+heading.style.minHeight = (window.innerHeight - nav.offsetHeight) + "px";
+content.style.minHeight = (window.innerHeight - footer.offsetHeight) + "px";
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90cy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsSUFBSSxPQUFPLEdBQWlCLFFBQVEsQ0FBQyxjQUFjLENBQUMsZUFBZSxDQUFDLENBQUM7QUFDckUsSUFBSSxPQUFPLEdBQWlCLFFBQVEsQ0FBQyxjQUFjLENBQUMsZUFBZSxDQUFDLENBQUM7QUFDckUsSUFBSSxHQUFHLEdBQWlCLFFBQVEsQ0FBQyxjQUFjLENBQUMsYUFBYSxDQUFDLENBQUM7QUFDL0QsSUFBSSxNQUFNLEdBQWlCLFFBQVEsQ0FBQyxjQUFjLENBQUMsY0FBYyxDQUFDLENBQUM7QUFDbkUsT0FBTyxDQUFDLEtBQUssQ0FBQyxTQUFTLEdBQUcsQ0FBQyxNQUFNLENBQUMsV0FBVyxHQUFHLEdBQUcsQ0FBQyxZQUFZLENBQUMsR0FBRyxJQUFJLENBQUM7QUFDekUsT0FBTyxDQUFDLEtBQUssQ0FBQyxTQUFTLEdBQUksQ0FBQyxNQUFNLENBQUMsV0FBVyxHQUFHLE1BQU0sQ0FBQyxZQUFZLENBQUMsR0FBRyxJQUFJLENBQUMifQ==
--- ts/index.ts
+++ ts/index.ts
@@ -1,2 +1,6 @@
-let message : string = "Hello World!";
-console.log(message);
+let heading : HTMLElement = document.getElementById('heading-panel');
+let content : HTMLElement = document.getElementById('content-panel');
+let nav : HTMLElement = document.getElementById('main-navbar');
+let footer : HTMLElement = document.getElementById('footer-panel');
+heading.style.minHeight = (window.innerHeight - nav.offsetHeight) + "px";
+content.style.minHeight =  (window.innerHeight - footer.offsetHeight) + "px";
